31aea20f72317bd115210788e78d8c847f55b1ac
Egor Rasskazov <rasskazov.note@gmail.com>
2022-06-15 16:13:33 +0300

    add Brame Game Prime

---
+++ src/Games/Prime.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace BrainGames\Games\Prime;
+
+use function BrainGames\Engine\startGame;
+
+function generateQuestionPrime($numberOfRounds = 3)
+{
+    return rand(0, 100);
+}
+
+function isPrime($number)
+{
+    $divider = 2;
+    for ($i = $divider; $i < $number; $i++) {
+        if ($number % $i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function generateDataPrime($numberOfRounds = 3)
+{
+    $questions = [];
+    $answers = [];
+    for ($i = 0; $i < $numberOfRounds; $i++) {
+        $questions[] = generateQuestionPrime();
+        $answers[] = isPrime($questions[$i]) ? 'yes' : 'no';
+    }
+    return [$questions, $answers];
+}
+
+function startPrimeGame()
+{
+    $questionMessage = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+    [$questions, $answers] = generateDataPrime();
+    startGame($questionMessage, $questions, $answers);
+}
